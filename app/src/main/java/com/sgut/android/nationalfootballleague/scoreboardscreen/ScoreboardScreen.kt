806f0e88569f9bc38b9554ea7c56ee5c8d121e05
salgut408 <sgutie@saic.edu>
2022-11-24 09:42:39 -0600

    scoreboard screen frmat

--- app/src/main/java/com/sgut/android/nationalfootballleague/scoreboardscreen/ScoreboardScreen.kt
+++ app/src/main/java/com/sgut/android/nationalfootballleague/scoreboardscreen/ScoreboardScreen.kt
@@ -2,6 +2,7 @@ package com.sgut.android.nationalfootballleague.scoreboardscreen
 
 import android.util.Log
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -28,6 +29,7 @@ fun ScoreboardScreen(
     Column(
         modifier
             .verticalScroll(rememberScrollState())
+            .padding(16.dp)
     ) {
         Text(text = scoreboardUiState.scoreboardUiStateEvents.day?.date ?: "", style = MaterialTheme.typography.displayLarge)
         Text(text = "Headlines", style = MaterialTheme.typography.displaySmall)
@@ -39,11 +41,23 @@ fun ScoreboardScreen(
 
         for (i in scoreboardUiState.scoreboardUiStateEvents.events ) {
             Text(text = i.shortName  ?: "", style = MaterialTheme.typography.headlineMedium)
+            Row(modifier.padding(16.dp)) {
+                i.competitions[0].competitors[0].team?.name?.let { Text(text = it, style = MaterialTheme.typography.headlineMedium) }
+                i.competitions[0].competitors[1].team?.name?.let { Text(text = it, style = MaterialTheme.typography.headlineMedium) }
+            }
+            Row(modifier.padding(16.dp)){
+                Text(text = i.competitions[0].competitors[0].score.toString() ,style = MaterialTheme.typography.headlineMedium)
+                Text(text = "-", style = MaterialTheme.typography.headlineMedium)
+                Text(text = i.competitions[0].competitors[1].score.toString() ,style = MaterialTheme.typography.headlineMedium)
+            }
+            Row(modifier.padding(16.dp)){
+                Text(text = i.competitions[0].competitors[0].team?.color ?: "", style = MaterialTheme.typography.headlineMedium)
+                Text(text = i.competitions[0].competitors[1].team?.color ?: "", style = MaterialTheme.typography.headlineMedium)
+            }
             Text(text =  i.competitions[0].competitors[0].team?.name + " " + i.competitions[0].competitors[0].score.toString()  ?: "", style = MaterialTheme.typography.headlineMedium)
             Text(text =  i.competitions[0].competitors[1].team?.name + " " + i.competitions[0].competitors[1].score.toString()  ?: "", style = MaterialTheme.typography.headlineMedium)
 
-            Text(text = i.competitions[0].competitors[0].team?.color ?: "", style = MaterialTheme.typography.headlineMedium)
-            Text(text = i.competitions[0].competitors[1].team?.color ?: "", style = MaterialTheme.typography.headlineMedium)
+
 
             Spacer(modifier = Modifier.padding(16.dp))
 
