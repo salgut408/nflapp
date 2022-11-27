7cfa5d426f7d47241380d424d2f0d76a3db296f1
salgut408 <sgutie@saic.edu>
2022-11-27 10:41:51 -0600

    Formatting

--- app/src/main/java/com/sgut/android/nationalfootballleague/data/domainmodels/ScoreboardResponseEventModel.kt
+++ app/src/main/java/com/sgut/android/nationalfootballleague/data/domainmodels/ScoreboardResponseEventModel.kt
@@ -3,14 +3,13 @@ package com.sgut.android.nationalfootballleague.data.domainmodels
 import com.google.gson.annotations.SerializedName
 import com.sgut.android.nationalfootballleague.*
 
-data class ScoreboardResponseEventModel (
+data class ScoreboardResponseEventModel(
 
 
-    var leagues : ArrayList<Leagues> = arrayListOf(),
-    var season  : SeasonScoreboard?            = SeasonScoreboard(),
-   var day     : DayScoreboard?               = DayScoreboard(),
-     var events  : List<EventsScoreboard>  = listOf()
-
+    var leagues:    ArrayList<Leagues>     = arrayListOf(),
+    var season:     SeasonScoreboard?      = SeasonScoreboard(),
+    var day:        DayScoreboard?         = DayScoreboard(),
+    var events:     List<EventsScoreboard> = listOf(),
 
 
     )
