b778d3a4628d5a9eb41134224d732cd63f828286
salgut408 <sgutie@saic.edu>
2022-11-27 11:15:31 -0600

    Dropdown menu addded

---
+++ app/src/main/java/com/sgut/android/nationalfootballleague/atheletedetail/AtheleteDetailSceenUiState.kt
@@ -0,0 +1,7 @@
+package com.sgut.android.nationalfootballleague.atheletedetail
+
+data class AtheleteDetailSceenUiState(
+    val currentSport: String = "",
+    val currentLeague: String = "",
+    val currentAthelete: String = ""
+)
