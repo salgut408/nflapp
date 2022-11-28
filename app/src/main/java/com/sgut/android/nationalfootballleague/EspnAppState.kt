e3e4df4cd40907179be7f2f5ec66659cb94a2fee
salgut408 <sgutie@saic.edu>
2022-11-28 11:55:07 -0600

    AppState added

--- app/src/main/java/com/sgut/android/nationalfootballleague/EspnAppState.kt
+++ app/src/main/java/com/sgut/android/nationalfootballleague/EspnAppState.kt
@@ -1,10 +1,60 @@
 package com.sgut.android.nationalfootballleague
 
+import android.content.res.Resources
 import androidx.compose.material.ScaffoldState
 import androidx.compose.runtime.Stable
+import androidx.navigation.NavHostController
+import com.sgut.android.nationalfootballleague.commoncomposables.snackbar.SnackbarManager
+import com.sgut.android.nationalfootballleague.commoncomposables.snackbar.SnackbarMessage.Companion.toMessage
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.launch
 
 //@Stable
- class EspnAppState(
-  val scaffoldState: ScaffoldState,
+class EspnAppState(
+ val scaffoldState: ScaffoldState,
+ val navController: NavHostController,
+ private val snackbarManager: SnackbarManager,
+ private val resources: Resources,
+ coroutineScope: CoroutineScope,
+) {
+    init {
+        coroutineScope.launch {
+            snackbarManager.snackbarMessages
+                .filterNotNull()
+                .collect { snackbarMessage ->
+                    val text = snackbarMessage.toMessage(resources)
+                    scaffoldState.snackbarHostState.showSnackbar(text)
+                }
+        }
+    }
 
- )
+    fun popUp() {
+        navController.popBackStack()
+    }
+
+    fun navigate(route: String) {
+        navController.navigate(route) { launchSingleTop = true }
+    }
+
+    fun navigateAndPopUp(route: String, popUp: String) {
+        navController.navigate(route) {
+            launchSingleTop = true
+            popUpTo(popUp) { inclusive = true }
+        }
+    }
+
+    fun clearAndNavigate(route: String) {
+        navController.navigate(route) {
+            launchSingleTop = true
+            popUpTo(0) {
+                inclusive = true
+            }
+        }
+    }
+
+
+}
+
+
