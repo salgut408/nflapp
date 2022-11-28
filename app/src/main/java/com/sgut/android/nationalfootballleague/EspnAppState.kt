ab28444250d4812c0c6da11c0610274b103ed7a9
salgut408 <sgutie@saic.edu>
2022-11-28 11:29:58 -0600

    Refactor Navigation.kt

---
+++ app/src/main/java/com/sgut/android/nationalfootballleague/EspnAppState.kt
@@ -0,0 +1,10 @@
+package com.sgut.android.nationalfootballleague
+
+import androidx.compose.material.ScaffoldState
+import androidx.compose.runtime.Stable
+
+//@Stable
+ class EspnAppState(
+  val scaffoldState: ScaffoldState,
+
+ )
