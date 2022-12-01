1172ed156de1a734a4f7a1d100a460d59ebe8195
salgut408 <sgutie@saic.edu>
2022-12-01 15:00:19 -0600

    League added

---
+++ app/src/main/java/com/sgut/android/nationalfootballleague/commoncomposables/snackbar/TextSnackbarContainer.kt
@@ -0,0 +1,52 @@
+package com.sgut.android.nationalfootballleague.commoncomposables.snackbar
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun TextSnackbarContainer(
+    snackbarText: String,
+    showSnackbar: Boolean,
+    onDismissSnackbar: () -> Unit,
+    modifier: Modifier = Modifier,
+    snackbarHostState: SnackbarHostState = remember { SnackbarHostState() },
+    content: @Composable () -> Unit
+) {
+    Box(modifier) {
+        content()
+
+        val onDismissState by rememberUpdatedState(onDismissSnackbar)
+        LaunchedEffect(showSnackbar, snackbarText) {
+            if (showSnackbar) {
+                try {
+                    snackbarHostState.showSnackbar(
+                        message = snackbarText,
+                        duration = SnackbarDuration.Short
+                    )
+                } finally {
+                    onDismissState()
+                }
+            }
+        }
+
+        // Override shapes to not use the ones coming from the MdcTheme
+        MaterialTheme(shapes = Shapes()) {
+            SnackbarHost(
+                hostState = snackbarHostState,
+                modifier = modifier
+                    .align(Alignment.BottomCenter)
+                    .systemBarsPadding()
+                    .padding(all = 8.dp),
+            ) {
+                Snackbar(it)
+            }
+        }
+    }
+}
