6c87b8cf6dbfdef11e9ef699f6c3366cd5034e2d
salgut408 <sgutie@saic.edu>
2022-12-09 16:10:05 -0600

    Scoreboard edits

--- app/src/main/java/com/sgut/android/nationalfootballleague/scoreboardscreen/ScoreboardScreen.kt
+++ app/src/main/java/com/sgut/android/nationalfootballleague/scoreboardscreen/ScoreboardScreen.kt
@@ -1,11 +1,13 @@
 package com.sgut.android.nationalfootballleague.scoreboardscreen
 
 import android.util.Log
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import com.sgut.android.nationalfootballleague.R.string as AppText
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -13,12 +15,21 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
+import com.sgut.android.nationalfootballleague.CompetitionsScoreboard
+import com.sgut.android.nationalfootballleague.CompetitorsScoreboard
 import com.sgut.android.nationalfootballleague.TeamScoreboard
 import com.sgut.android.nationalfootballleague.commoncomposables.BasicButton
 import com.sgut.android.nationalfootballleague.commoncomposables.TeamLogoImageloadiner2
@@ -26,6 +37,7 @@ import com.sgut.android.nationalfootballleague.commoncomposables.TeamLogoScorebo
 import com.sgut.android.nationalfootballleague.homelistscreen.ArticleCard
 import com.sgut.android.nationalfootballleague.homelistscreen.ArticleList
 import com.sgut.android.nationalfootballleague.homelistscreen.ArticleRow
+import com.sgut.android.nationalfootballleague.teamdetails.HexToJetpackColor2
 import com.sgut.android.nationalfootballleague.utils.basicButton
 import com.sgut.android.nationalfootballleague.utils.card
 import com.sgut.android.nationalfootballleague.utils.spacer
@@ -89,33 +101,53 @@ fun ScoreboardScreen(
 
 
         for (i in events) {
-            Card(modifier.padding(8.dp)) {
-                BasicButton(text = AppText.watc, modifier = Modifier.basicButton()) {
-                }
+            Card(modifier
+                .padding(8.dp)
+                .clickable {}
+            ) {
 
-                Text(text = i.name ?: "", style = MaterialTheme.typography.bodyLarge)
-                Text(text = i.id ?: "", style = MaterialTheme.typography.bodyLarge)
+//                BasicButton(text = AppText.watc, modifier = Modifier.basicButton()) { }
+//                Text(text = i.name ?: "", style = MaterialTheme.typography.bodyLarge)
+//                Text(text = i.id ?: "", style = MaterialTheme.typography.bodyLarge)
 
-                for(i in i.competitions[0].competitors) {
 
-                    TeamComponent(team = i.team!!, modifier = Modifier)
-                    Divider(modifier = Modifier.padding(3.dp))
 
-                }
+                    for (j in i.competitions[0].competitors) {
+
+                        TeamComponent(team = j, modifier = Modifier)
+                        Divider(modifier = Modifier.padding(3.dp))
+
+                    }
+
+
+
 
 
                 Row(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.SpaceBetween,
-                    modifier = modifier
-                        .padding(8.dp)
-                        .fillMaxWidth()
                 ) {
+
+//                    Row(){}
 
                     //use id for game details screen
                     Text("Period ", style = MaterialTheme.typography.bodyLarge)
 
                     Text(text = i.status.period.toString(), style = MaterialTheme.typography.bodyLarge)
+
+
+                    FloatingActionButton(
+                        onClick = { /*TODO*/ },
+                        contentColor = Color.White,
+                        modifier = Modifier
+                            .size(30.dp)
+
+                    ) {
+                        Icon(Icons.Outlined.Star, "null")
+                    }
 
 
                 }
@@ -139,23 +171,44 @@ fun ScoreboardScreen(
 
 
 @Composable
-fun TeamComponent(team: TeamScoreboard, modifier: Modifier) {
-    Box(modifier=modifier.padding(16.dp).fillMaxWidth(2f)) {
+fun TeamComponent(team: CompetitorsScoreboard, modifier: Modifier) {
+    val color = HexToJetpackColor2.getColor(team.team?.color!!)
+
+
+    Box(modifier= modifier
+        .padding(16.dp)
+        .background( Brush.horizontalGradient(
+            listOf(color, Color.White)
+        ))
+        .fillMaxWidth(3f)) {
 
 
 
         Row(
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically,
-            modifier = modifier.fillMaxWidth().height(50.dp)
+            modifier = modifier
+                .fillMaxWidth()
+                .height(50.dp)
         ){
-            TeamLogoScoreboardImageLoader(team = team )
+            TeamLogoScoreboardImageLoader(team = team.team!! )
 
-            Text(text = team.shortDisplayName)
-            Text(text = team.score.toString())
+
+
+            Text(
+                text = team.team!!.name,
+                style = TextStyle(fontSize = 20.sp),
+                textAlign = TextAlign.Left,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = team.score.toString(),
+                style = TextStyle(fontSize = 20.sp),
+                textAlign = TextAlign.Left,
+                fontWeight = FontWeight.Bold
+            )
         }
-//        Column(modifier = modifier.width(50.dp)){
-//
-//        }
+
     }
 }
+
