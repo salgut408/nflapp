51bf28b203e829e0f72c627c49985a77940cada3
salgut408 <sgutie@saic.edu>
2023-03-10 10:37:05 -0600

    Added /v2 to api endpoints

--- app/src/main/java/com/sgut/android/nationalfootballleague/data/remote/api/EspnApi.kt
+++ app/src/main/java/com/sgut/android/nationalfootballleague/data/remote/api/EspnApi.kt
@@ -8,16 +8,16 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface EspnApi {
-    @GET("sports/football/nfl/teams")
+    @GET("v2/sports/football/nfl/teams")
     suspend fun getAllNflTeams(): Response<NFLTeamsResponse>
 
-    @GET("sports/football/nfl/teams/{teamAbbreviation}?enable=roster,headshot")
+    @GET("v2/sports/football/nfl/teams/{teamAbbreviation}?enable=roster,headshot")
     suspend fun getSpecificNflTeam(
         @Path("teamAbbreviation")
         teamAbbreviation: String? = null
     ): Response<TeamDetailResponse2>
 
-    @GET("sports/{sport}/{league}/teams/{teamAbbreviation}?enable=roster,headshot")
+    @GET("v2/sports/{sport}/{league}/teams/{teamAbbreviation}?enable=roster,headshot")
     suspend fun getSpecificTeam(
         @Path("sport")
         sport: String? = null,
@@ -27,43 +27,43 @@ interface EspnApi {
         teamAbbreviation: String? = null
     ): Response<TeamDetailResponse2>
 
-    @GET("sports/football/college-football/teams")
+    @GET("v2/sports/football/college-football/teams")
     suspend fun getAllCollegeTeams(): Response<NFLTeamsResponse>
 
-    @GET("sports/baseball/mlb/teams")
+    @GET("v2/sports/baseball/mlb/teams")
     suspend fun getAllBaseballTeams(): Response<NFLTeamsResponse>
 
-    @GET("sports/hockey/nhl/teams")
+    @GET("v2/sports/hockey/nhl/teams")
     suspend fun getAllHockeyTeams(): Response<NFLTeamsResponse>
 
-    @GET("sports/basketball/wnba/teams")
+    @GET("v2/sports/basketball/wnba/teams")
     suspend fun getAllWomensBasketballTeams(): Response<NFLTeamsResponse>
 
-    @GET("sports/basketball/nba/teams")
+    @GET("v2/sports/basketball/nba/teams")
     suspend fun getAllBasketballTeams(): Response<NFLTeamsResponse>
 
-    @GET("sports/soccer/usa.1/teams")
+    @GET("v2/sports/soccer/usa.1/teams")
     suspend fun getAllSoccerTeams(): Response<NFLTeamsResponse>
 
-    @GET("sports/soccer/uefa.europa/teams")
+    @GET("v2/sports/soccer/uefa.europa/teams")
     suspend fun getAllUefaSoccerTeams(): Response<NFLTeamsResponse>
 
-    @GET("sports/soccer/fifa.world/teams")
+    @GET("v2/sports/soccer/fifa.world/teams")
     suspend fun getAllFifaSoccerTeams(): Response<NFLTeamsResponse>
 
-    @GET("sports/basketball/mens-college-basketball/teams")
+    @GET("v2/sports/basketball/mens-college-basketball/teams")
     suspend fun getAllCollegeBasketballTeams(): Response<NFLTeamsResponse>
 
-    @GET("sports/soccer/eng.1/teams")
+    @GET("v2/sports/soccer/eng.1/teams")
     suspend fun getAllEnglishSoccerTeams(): Response<NFLTeamsResponse>
 
-    @GET("sports/soccer/esp.1/teams")
+    @GET("v2/sports/soccer/esp.1/teams")
     suspend fun getAllSpanishSoccerTeams(): Response<NFLTeamsResponse>
 
-    @GET("sports/football/nfl/scoreboard")
+    @GET("v2/sports/football/nfl/scoreboard")
     suspend fun getWorldCupScoreboard(): Response<NetworkScoreboardResponse>
 
-    @GET("sports/{sport}/{league}/news")
+    @GET("v2/sports/{sport}/{league}/news")
     suspend fun getArticles(
         @Path("sport")
         sport: String? = null,
@@ -71,7 +71,7 @@ interface EspnApi {
         league: String? = null,
     ): Response<NetworkArticleResponse>
 
-    @GET("sports/{sport}/{league}/scoreboard")
+    @GET("v2/sports/{sport}/{league}/scoreboard")
     suspend fun getGeneralScoreboard(
         @Path("sport")
         sport: String? = null,
@@ -79,7 +79,7 @@ interface EspnApi {
         league: String? = null,
     ): Response<NetworkScoreboardResponse>
 
-    @GET("sports/{sport}/{league}/scoreboard")
+    @GET("v2/sports/{sport}/{league}/scoreboard")
     suspend fun getYesterdayGeneralScoreboard(
         @Path("sport")
         sport: String? = null,
@@ -88,7 +88,7 @@ interface EspnApi {
         @Query("week") week: Int
     ): Response<NetworkScoreboardResponse>
 
-    @GET("sports/{sport}/{league}/teams")
+    @GET("v2/sports/{sport}/{league}/teams")
     suspend fun getTeamsListForLeague(
         @Path("sport")
         sport: String? = null,
@@ -99,7 +99,7 @@ interface EspnApi {
     //teams ?
 
 
-    @GET("sports/{sport}/{league}/teams/{teamAbbreviation}?enable=roster,headshot")
+    @GET("v2/sports/{sport}/{league}/teams/{teamAbbreviation}?enable=roster,headshot")
     suspend fun getSpecifcTeam(
         @Path("sport")
         sport: String? = null,
@@ -109,7 +109,7 @@ interface EspnApi {
         teamAbbreviation: String? = null
     ): Response<TeamDetailResponse2>
 
-    @GET("sports/{sport}/{league}/summary")
+    @GET("v2/sports/{sport}/{league}/summary")
     suspend fun getGameDetails(
         @Path("sport")
         sport: String? = null,
@@ -125,7 +125,7 @@ interface EspnApi {
     ): Response<ArticleDetailNetworkResponse>
 
 // TODO implement getting scoreboard with dates
-    @GET("sports/{sport}/{league}/scoreboard")
+    @GET("v2/sports/{sport}/{league}/scoreboard")
     suspend fun getGeneralScoreboardWithDate(
         @Path("sport")
         sport: String? = null,
@@ -135,7 +135,7 @@ interface EspnApi {
     ): Response<NetworkScoreboardResponse>
 
 // TODO implement team Schedule
-    @GET("sports/{sport}/{league}/teams/{teamId}schedule")
+    @GET("v2/sports/{sport}/{league}/teams/{teamId}schedule")
     suspend fun getTeamSchedule(
         @Path("sport")
         sport: String? = null,
@@ -148,5 +148,9 @@ interface EspnApi {
 
     // TODO implement racing API Endpoint F1
 //    https://site.api.espn.com/apis/site/v2/sports/racing/f1/scoreboard
+
+    // TODO add STATS
+//    https://site.api.espn.com/apis/site/v2/sports/soccer/eng.1/statistics
+
 
 }
