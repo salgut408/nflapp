bf738ce193c3d656ce5bd451291fa60a74799054
salgut408 <sgutie@saic.edu>
2023-05-25 15:27:21 -0500

    Saving

---
+++ app/src/main/java/com/sgut/android/nationalfootballleague/data/db/leaders/LeadersDao.kt
@@ -0,0 +1,9 @@
+package com.sgut.android.nationalfootballleague.data.db.leaders
+
+import androidx.room.Dao
+
+@Dao
+interface LeadersDao {
+//    @Insert(onConflict = OnConflictStrategy.NONE)
+//    suspend fun addLeaders(leadersDbObj: List<GameDetailsLeadersDb>): List<Long>
+}
